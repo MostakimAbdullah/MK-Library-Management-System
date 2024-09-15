b50062cb3866ac422e0c40e71efbf60177a9f77c
unknown <mostakimabdullah2@gmail.com>
2024-09-15 23:23:40 +0600

    changes again again done

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from useraccount.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', home, name='home'),
     path('accounts/', include('useraccount.urls')),
     path('books_details/', include('book.urls')),
 ]
--- useraccount/urls.py
+++ useraccount/urls.py
@@ -5,7 +5,7 @@ urlpatterns =[
     path('register/', RegisterView , name='registration'),
     path('login/', LoginView , name='login'),
     path('logout/', Logoutview , name='logout'),
-    path('home/', home, name='home'),
+    #path('home/', home, name='home'),
     path('category/<slug:category_slug>', home , name='category_wise_slug'),
     path('deposit/', depositmoney , name='Deposit'),
 ]
